1a1de7938d8c6a184221ed42dfe3af644b73c11c
Hakkim Azlan <mnorhakkimnorazlan@gmail.com>
2023-01-16 16:28:57 +0800

    My first change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today is rainy day");
+        System.out.println("I hate Jenkins");
     }
 
     public String getMessage() {
