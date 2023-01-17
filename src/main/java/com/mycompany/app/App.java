35ce31ac3b5ecf125a5488d9fb1ba239b563bfdd
Hakkim Azlan <mnorhakkimnorazlan@gmail.com>
2023-01-17 09:51:50 +0800

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I still hate Jenkins");
+        System.out.println("I don't know what to say");
     }
 
     public String getMessage() {
